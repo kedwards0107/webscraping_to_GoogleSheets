228bbcb2f17b8e7da1e874e0417cae49a60de4e1
Kevin Edwards <31578240+kedwards0107@users.noreply.github.com>
2022-04-29 20:17:07 -0400

    Update webscraping_to_google.py

--- webscraping_to_google.py
+++ webscraping_to_google.py
@@ -14,6 +14,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
+# Need to setup google sheet by using 
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds2.json", scope)
 
 client = gspread.authorize(creds)
